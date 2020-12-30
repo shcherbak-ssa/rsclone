8b8519076e83f4d7793a3c7c80e38e4a58021026
shcherbak <shcherbak.ssa@gmail.com>
2020-12-30 10:27:26 +0300

    feat: create inputs.store

---
+++ src/auth/store/inputs.store.ts
@@ -0,0 +1,97 @@
+import {
+  NAME_INPUT_LABEL,
+  EMAIL_INPUT_LABEL,
+  PASSWORD_INPUT_LABEL,
+} from '../constants';
+
+enum Constants {
+  UPDATE_VALUE = 'inputs-store/update-value',
+  UPDATE_ERROR = 'inputs-store/update-error',
+};
+
+type InputLabelType =
+  typeof NAME_INPUT_LABEL |
+  typeof EMAIL_INPUT_LABEL |
+  typeof PASSWORD_INPUT_LABEL;
+
+type UpdateValueActionType = {
+  type: typeof Constants.UPDATE_VALUE,
+  value: string,
+  inputLabel: InputLabelType,
+};
+
+type UpdateErrorActionType = {
+  type: typeof Constants.UPDATE_ERROR,
+  error: string,
+  inputLabel: InputLabelType,
+};
+
+export type InputActionType = UpdateValueActionType | UpdateErrorActionType;
+
+export type InputsState = {};
+
+const initialState: InputsState = {
+  [NAME_INPUT_LABEL]: {
+    value: '',
+    error: '',
+    inputLabel: NAME_INPUT_LABEL,
+  },
+  [EMAIL_INPUT_LABEL]: {
+    value: '',
+    error: '',
+    inputLabel: EMAIL_INPUT_LABEL,
+  },
+  [PASSWORD_INPUT_LABEL]: {
+    value: '',
+    error: '',
+    inputLabel: PASSWORD_INPUT_LABEL,
+  },
+}
+
+function inputsReducer(
+  state: InputsState = initialState,
+  action: InputActionType,
+): InputsState {
+  switch (action.type) {
+    case Constants.UPDATE_VALUE:
+      return {
+        ...state,
+        [action.inputLabel]: {
+          value: action.value
+        }
+      };
+    case Constants.UPDATE_ERROR:
+      return {
+        ...state,
+        [action.inputLabel]: {
+          error: action.error
+        }
+      };
+    default:
+      return state;
+  }
+}
+
+function updateValue(
+  value: string, inputLabel: InputLabelType,
+): UpdateValueActionType {
+  return {
+    type: Constants.UPDATE_VALUE, value, inputLabel,
+  };
+}
+
+function updateError(
+  error: string, inputLabel: InputLabelType,
+): UpdateErrorActionType {
+  return {
+    type: Constants.UPDATE_ERROR, error, inputLabel,
+  };
+}
+
+export const inputsStore = {
+  reducer: inputsReducer,
+  actions: {
+    updateValue,
+    updateError,
+  },
+};
