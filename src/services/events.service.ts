0783900072d42a98f6194e473802e058e84de6c9
shcherbak <shcherbak.ssa@gmail.com>
2021-01-04 05:40:58 +0300

    feat: create events.service

---
+++ src/services/events.service.ts
@@ -0,0 +1,49 @@
+const EVENTS_ERROR: string = 'EventsError';
+
+class EventsError implements Error {
+  name: string = EVENTS_ERROR;
+  message: string;
+
+  constructor(message: string) {
+    this.message = message;
+  }
+}
+
+export class Events {
+  private events: Map<string, Array<Function>> = new Map();
+
+  on(event: string, handler: Function) {
+    const eventHandlers: Array<Function> = this.getEventHandlers(event);
+    eventHandlers.push(handler);
+    
+    this.events.set(event, eventHandlers);
+  }
+
+  off(event: string, handler: Function) {
+    if (!this.isEventExist(event)) return;
+
+    const eventHandlers: Array<Function> = this.getEventHandlers(event);
+    this.removeHandler(eventHandlers, handler);
+  }
+
+  emit(event: string, payload: any) {
+    if (!this.isEventExist(event)) {
+      throw new EventsError(`Event '${event}' does not exist`);
+    }
+
+    const eventHandlers: Array<Function> = this.getEventHandlers(event);
+    eventHandlers.forEach((handler) => handler(payload));
+  }
+
+  private getEventHandlers(event: string): Array<Function> {
+    return this.events.get(event) || [];
+  }
+
+  private isEventExist(event: string) {
+    return this.events.has(event);
+  }
+
+  private removeHandler(eventHandlers: Array<Function>, handler: Function) {
+    eventHandlers.filter((eventHandler) => eventHandler !== handler);
+  }
+}
