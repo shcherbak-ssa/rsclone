2ae197c7cecd1fab4cc33f00971aa9b2f00aeae6
shcherbak <shcherbak.ssa@gmail.com>
2021-01-05 23:34:46 +0300

    refactor: replace password input with settings-action in settings-login component

--- src/app/components/settings-login/settings-login.tsx
+++ src/app/components/settings-login/settings-login.tsx
@@ -3,13 +3,13 @@ import { useSelector } from 'react-redux';
 import './settings-login.scss';
 
 import { SettingsSection, SettingsSectionProps } from '../../containers/settings-section';
+import { SettingsAction, SettingsActionProps } from '../../containers/settings-action';
 import { Base, BaseInputProps } from '../base';
 import { storeSelectors } from '../../store';
 
 export function SettingsLogin() {
   const {email} = useSelector(storeSelectors.user.get());
   const [emailValue, setEmailValue] = useState(email);
-  const [currentPasswordValue, setCurrentPasswordValue] = useState('');
 
   const settingsSectionProps: SettingsSectionProps = {
     isActive: true,
@@ -22,20 +22,24 @@ export function SettingsLogin() {
     value: emailValue,
     placeholder: 'E-mail',
     error: '',
-    updateValue: (value: string) => {},
+    updateValue: (value: string) => {
+      setEmailValue(value);
+    },
   };
 
-  const currentPasswordInputProps: BaseInputProps = {
-    value: currentPasswordValue,
-    placeholder: 'Password',
-    error: '',
-    updateValue: (value: string) => {},
+  const settingsActionProps: SettingsActionProps = {
+    title: 'Password',
+    description: 'Secure your GitBook Clone account with a strong and unique password',
+    buttonProps: {
+      value: 'Change password',
+      clickHandler: () => {},
+    },
   };
 
   return (
     <SettingsSection {...settingsSectionProps}>
       <Base.Input {...emailInputProps} />
-      <Base.Input {...currentPasswordInputProps} />
+      <SettingsAction {...settingsActionProps} />
     </SettingsSection>
   );
 }
