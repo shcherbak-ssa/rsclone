678b0caca4e14aa036ea600b6d09bf7656574a30
shcherbak <shcherbak.ssa@gmail.com>
2021-01-06 10:53:13 +0300

    refactor: update network.service

--- src/app/models/user-initer.model.ts
+++ src/app/models/user-initer.model.ts
@@ -1,39 +1,24 @@
-import { USER_LOCALSTORAGE_LABEL, SUCCESS_RESPONSE_TYPE } from "../../constants";
-import { AppRoutesService } from "../../services/app-routes.service";
-import { LocalStorageService } from "../../services/localstorage.service";
+import { SUCCESS_RESPONSE_TYPE } from "../../constants";
 import { NetworkService } from "../../services/network.service";
 import { dispatchAction } from "../store";
 import { UserStateType, userStore } from "../store/user.store";
 
-const USER_ID_URL_PARAM_LABEL: string = 'id';
-
 export class UserIniterModel {
   async loadUserData() {
-    const localStorageService: LocalStorageService = new LocalStorageService();
-    const urlParams = new URLSearchParams();
-
-    const localStorageUser = localStorageService.get(USER_LOCALSTORAGE_LABEL);
-    urlParams.append(USER_ID_URL_PARAM_LABEL, localStorageUser.userID);
-
-    const appRoutesService = new AppRoutesService();
-    const pathname = appRoutesService.getRootRoutePath();
-
-    const loadUserUrl = `${pathname}?${urlParams.toString()}`;
-    const response = await this.load(loadUserUrl);
-
+    const response = await this.load();
     this.updateUserStoreStates(response.user);
   }
 
-  private updateUserStoreStates(user: UserStateType) {
-    dispatchAction(userStore.actions.updateStates(user));
-  }
-
-  private async load(loadUserUrl: string) {
+  private async load() {
     const networkService: NetworkService = new NetworkService();
-    const response = await networkService.get(loadUserUrl);
+    const response = await networkService.get();
 
     if (response.type === SUCCESS_RESPONSE_TYPE) {
       return response.payload;
     }
+  }
+
+  private updateUserStoreStates(user: UserStateType) {
+    dispatchAction(userStore.actions.updateStates(user));
   }
 }
--- src/services/network.service.ts
+++ src/services/network.service.ts
@@ -1,9 +1,67 @@
+import { USER_LOCALSTORAGE_LABEL } from "../constants";
+import { AppRoutesService } from "./app-routes.service";
+import { LocalStorageService } from "./localstorage.service";
+
 const JSON_CONTENT_TYPE: string = 'application/json';
 const NETWORK_ERROR: string = 'NetworkError';
+const USER_ID_URL_PARAM_LABEL: string = 'id';
 
 enum Methods {
   GET = 'GET',
+  DELETE = 'DELETE',
+  EMPTY = '',
 };
+
+type FetchRequestType = {
+  url: string;
+  method: Methods;
+  body?: any;
+  contentType: string;
+};
+
+class FetchRequest {
+  private url: string;
+  private method: Methods;
+  private body?: any;
+  private contentType: string;
+
+  constructor(method: Methods, body?: any) {
+    this.method = method;
+    this.body = body;
+
+    this.createUrl();
+    this.setContentType();
+  }
+
+  getRequestData(): FetchRequestType {
+    return {
+      url: this.url,
+      method: this.method,
+      body: this.body,
+      contentType: this.contentType,
+    };
+  }
+
+  private createUrl() {
+    const localStorageService: LocalStorageService = new LocalStorageService();
+    const urlParams = new URLSearchParams();
+
+    const localStorageUser = localStorageService.get(USER_LOCALSTORAGE_LABEL);
+    urlParams.append(USER_ID_URL_PARAM_LABEL, localStorageUser.userID);
+
+    const appRoutesService = new AppRoutesService();
+    const pathname = appRoutesService.getRootRoutePath();
+
+    this.url = `${pathname}?${urlParams.toString()}`;
+  }
+
+  private setContentType() {
+    if (typeof this.body === 'object') {
+      this.body = JSON.stringify(this.body);
+      this.contentType = JSON_CONTENT_TYPE;
+    }
+  }
+}
 
 export class NetworkError implements Error {
   name: string = NETWORK_ERROR;
@@ -15,20 +73,30 @@ export class NetworkError implements Error {
 }
 
 export class NetworkService {
-  async get(url: string = location.pathname) {
+  async get() {
+    return await this.sendRequest(Methods.GET);
+  }
+
+  private async sendRequest(method: Methods, body?: any) {
     try {
-      const response = await this.sendRequest(url, Methods.GET);
+      const fetchRequest = new FetchRequest(method, body);
+      const response = await this.fetchRequest(fetchRequest.getRequestData());
+
       return await response.json();
     } catch (error) {
       console.log(error);
     }
+    
   }
 
-  private async sendRequest(url: string, method: string, contentType?: string) {
+  private async fetchRequest({
+    url, method, body, contentType,
+  }: FetchRequestType) {
     return fetch(url, {
       method,
+      body,
       headers: {
-        'Content-Type': contentType || JSON_CONTENT_TYPE,
+        'Content-Type': contentType,
       }
     });
   }
