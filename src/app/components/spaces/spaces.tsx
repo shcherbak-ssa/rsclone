c1a1d38224755b7d31b50dce1364348c37a0d60e
shcherbak <shcherbak.ssa@gmail.com>
2021-01-08 15:15:48 +0300

    feat: add add space shortcut action

--- src/app/components/spaces/spaces.tsx
+++ src/app/components/spaces/spaces.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { useHotkeys } from 'react-hotkeys-hook';
 import './spaces.scss';
 
 import plusIcon from '@iconify/icons-ant-design/plus-outlined';
 
 import { HomepageSectionProps, HomepageSection } from '../../containers/homepage-section';
+import { storeStates } from '../../store';
 
 export function Spaces() {
+  const addSpaceKeyboadShortcut = storeStates.getAddSpaceKeyboardShortcut();
+
+  useHotkeys(addSpaceKeyboadShortcut.keys, () => {
+    console.log('Add space');
+  });
+
   const homepageSectionProps: HomepageSectionProps = {
     title: 'Spaces',
     buttonProps: {
--- src/app/store/store-states.ts
+++ src/app/store/store-states.ts
@@ -1,3 +1,10 @@
 import { store } from './store';
+import { ShortcutsLabels } from '../../../core/constants';
 
-export const storeStates = {};
+export const storeStates = {
+  getAddSpaceKeyboardShortcut() {
+    return store.getState().user.keyboardShortcuts.find((keyboardShortcut) => {
+      return keyboardShortcut.label === ShortcutsLabels.ADD_SPACE;
+    });
+  },
+};
